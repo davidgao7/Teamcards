d4b3e981e6a2f83f7eeb7b22250e10e53976581f
David Gao <gao.ten@northeastern.edu>
2024-06-09 11:07:45 -0400

    use page config from gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    base: "/Teamcards/",
+    mode: "development",
+    server: {
+        host: true, // add host port export
+        port: 3001,
+    }
 });
